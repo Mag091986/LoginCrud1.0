d719dc8442be6e63024d039942cb8d1f41da5455
Mag091986 <k9921star@gmail.com>
2024-08-19 18:20:35 -0500

    Encriptador

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -70,7 +70,7 @@ export const logout = (req, res) => {
 }
 
 export const profile = async (req, res) => {
-    const userFound = await  User.findById(req.user.id)
+    const userFound = await User.findById(req.user.id)
 
     if (!userFound) return res.status(400).json({message: "User not found"});
 
